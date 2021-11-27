dbc12fb445843f4c8a717262eecfe577234d83c3
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2021-11-27 13:00:30 +0000

    chore: Initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/base',
+        'plugin:vue/vue3-essential',
+        'plugin:vue/vue3-recommended',
+        'plugin:vue/vue3-strongly-recommended',
+        '@vue/typescript/recommended',
+        '@vue/airbnb',
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+        indent: ['error', 4],
+        'linebreak-style': 'off',
+        'eol-last': ['error', 'never'],
+        'max-len': ['error', { code: 450 }],
+        'space-before-function-paren': ['error', 'always'],
+
+        'vue/html-indent': ['error', 4, {
+            attribute: 1,
+            closeBracket: 0,
+            alignAttributesVertically: true,
+            ignores: [],
+        }],
+
+        'vue/html-closing-bracket-newline': ['error', {
+            singleline: 'never',
+            multiline: 'never',
+        }],
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)',
+            ],
+            env: {
+                jest: true,
+            },
+        },
+    ],
+};
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # vue-3-cli-typescript-scratch-space
-For writing one off Vue components that will be used elsewhere. For consistent formatting/linting/testing.
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your unit tests
+```
+npm run test:unit
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
